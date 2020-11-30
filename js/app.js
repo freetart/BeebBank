51d01ad471b4c6ddfcb0ef2d5040e86de2085e62
Chris M <habibdev96@gmail.com>
2020-11-30 06:07:39 -0700

    added modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
+// DOM ELEMENTS
 const menuIcon = document.querySelector(".hamburger-menu");
 const menuNavbar = document.querySelector(".menu-navbar");
+const close = document.getElementById("close");
+const open = document.querySelector(".open");
+const open2 = document.querySelector(".open2");
+const open3 = document.querySelector(".open3");
+const open4 = document.querySelector(".open4");
+const modal = document.getElementById("modal");
 
-// Hamburger menu
+// HAMBURGER MENU
 menuIcon.addEventListener("click", () => menuNavbar.classList.toggle("change"));
+
+// MODAL
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+open2.addEventListener("click", () => modal.classList.add("show-modal"));
+open3.addEventListener("click", () => modal.classList.add("show-modal"));
+open4.addEventListener("click", () => modal.classList.add("show-modal"));
+
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
